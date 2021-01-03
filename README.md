89f506faf0a8567a7dcd4618bde28bde20eeac6f
Manjunath Gowdar <57346550+Manjunath-Gowdar-au9@users.noreply.github.com>
2021-01-03 12:01:59 +0530

    handle input change wrt state

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
-# handling data in state
+# Handling data in state
 
-- display user from state data
+- display user from state data via toggle
 - delete perticular user on click
+- handle user input and change user data in state
+  
+  ![](https://media.giphy.com/media/yxSUYl58s2Z3sdPQme/giphy.gif)
+
+[Video](https://youtu.be/fbtn3phXwX40)
--- src/App.js
+++ src/App.js
@@ -1,63 +1,77 @@
-import React, { Component } from "react";
-import "./App.css";
-import Person from "./Person/Person";
+import React, { Component } from 'react';
+import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   state = {
     persons: [
-      { id: "uaoeua", name: "Max", age: 28 },
-      { id: "aoeuaoe", name: "Manu", age: 29 },
-      { id: "euaeou", name: "Stephanie", age: 26 },
+      { id: 'asfa1', name: 'Max', age: 28 },
+      { id: 'vasdf1', name: 'Manu', age: 29 },
+      { id: 'asdf11', name: 'Stephanie', age: 26 }
     ],
-    otherState: "some other value",
-    showPersons: false,
-  };
+    otherState: 'some other value',
+    showPersons: false
+  }
 
-  nameChangedHandler = (event) => {
-    this.setState({
-      persons: [
-        { name: "Max", age: 28 },
-        { name: event.target.value, age: 29 },
-        { name: "Stephanie", age: 26 },
-      ],
+  nameChangedHandler = ( event, id ) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+      // out of 3 id(received form DOM) compare it with state id, and return index of 1 matched user id
     });
-  };
+    
+
+    const person = {
+      ...this.state.persons[personIndex]
+      // copy user data to person
+    };
+    
+
+    person.name = event.target.value;
+    // copy target value to new person
+
+    const persons = [...this.state.persons];
+    // copy state
+    persons[personIndex] = person;
+    // add new person to new copied state
+
+    this.setState( {persons: persons} );
+    // update state with new state(persons)
+  }
 
   deletePersonHandler = (personIndex) => {
     const persons = [...this.state.persons];
     persons.splice(personIndex, 1);
-    this.setState({ persons: persons });
-  };
+    this.setState({persons: persons});
+  }
 
   togglePersonsHandler = () => {
     const doesShow = this.state.showPersons;
-    this.setState({ showPersons: !doesShow });
-  };
+    this.setState( { showPersons: !doesShow } );
+  }
 
-  render() {
+  render () {
     const style = {
-      backgroundColor: "white",
-      font: "inherit",
-      border: "1px solid blue",
-      padding: "8px",
-      cursor: "pointer",
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
     };
 
     let persons = null;
 
-    if (this.state.showPersons) {
+    if ( this.state.showPersons ) {
       persons = (
         <div>
           {this.state.persons.map((person, index) => {
-            return (
-              <Person
-                click={() => this.deletePersonHandler(index)}
-                name={person.name}
-                age={person.age}
-                key={person.id}
-                // {...console.log(person.id)}
-              />
-            );
+            return <Person
+              click={() => this.deletePersonHandler(index)}
+              name={person.name} 
+              age={person.age}
+              key={person.id}
+              changed={(event) => this.nameChangedHandler(event, person.id) } 
+               />
+               {/* receiving event and all user id from DOM, and passing it to method */}
           })}
         </div>
       );
@@ -65,10 +79,11 @@ class App extends Component {
 
     return (
       <div className="App">
-        <h1>Hi, I'm a Manjunath</h1>
-        <button style={style} onClick={this.togglePersonsHandler}>
-          Toggle Persons
-        </button>
+        <h1>Hi, I'm a React App</h1>
+        <p>This is really working!</p>
+        <button
+          style={style}
+          onClick={this.togglePersonsHandler}>Toggle Persons</button>
         {persons}
       </div>
     );
